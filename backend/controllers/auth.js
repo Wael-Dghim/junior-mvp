15a3b91cf074caf2ca12d74bec77da0f67aefeb1
Wael Dghim <104037074+Wael-Dghim@users.noreply.github.com>
2023-05-04 10:49:54 +0100

    signup controller

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -3,13 +3,41 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const login = async (req, res) => {
+const signup = async (req, res) => {
   const { fName, lName, username, password, email } = req.body;
+
+  try {
+    const userExists = await User.findOne({ $or: [{ email }, { username }] });
+    if (userExists) {
+      return res.send("Username/Email already exists");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      fName,
+      lName,
+      username,
+      password: hashedPassword,
+      email,
+    });
+    await newUser.save();
+    const user = await User.findOne({ username });
+    const token = jwt.sign({ id: user._id }, process.env.TOKEN);
+    res.set("Authorization", token);
+    return res.send("User created successfully");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const login = async (req, res) => {
+  const { username, password, email } = req.body;
 
   try {
     const user = await User.findOne({ $or: [{ email }, { username }] });
     if (!user) {
-      return res.status(400).send("Username/Email already exists");
+      return res.status(400).send("Incorrect username or email");
     }
 
     const correctPassword = await bcrypt.compare(password, user.password);
@@ -25,4 +53,4 @@ const login = async (req, res) => {
   }
 };
 
-module.exports = { login };
+module.exports = { signup, login };
